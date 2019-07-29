e1d0b7aa9d5b4c27497bfba73d78062816100137
Patrick Harrison <jahman@bellatlantic.net>
2019-07-29 15:02:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
 
+ def my_collect(languages)
+   
+    my_collect(collection) do |lang|
+    lang.upcase 
+    empty_array = []
+    i= 0
+  
+  while i < array.length
+  empty_array << yield(array[i])
+    i += 1
+end
+  
+  end
+  collection
+ end
+#   
+# end
+
+
 
